a1ef0615392cc9e169898734cd1ca07153d4879b
Julien Lavocat <jlavocat@leadformance.com>
2022-11-17 22:32:18 +0100

    ci: change production api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sprintr-api-prod.onrender.com/graphql',
-  wsApiUrl: 'wss://sprintr-api-prod.onrender.com/graphql',
+  apiUrl: 'https://sprintr-api-prod.cloud.julienlavocat.me/graphql',
+  wsApiUrl: 'wss://sprintr-api-prod.cloud.julienlavocat.me/graphql',
 };
